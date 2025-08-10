5fbe6ec823774363f939cc5bd7d958c6a0f25839
feliperdamaceno <feliperdamaceno@sent.com>
2025-08-11 00:13:17 +0100

    feat: include small pill option

--- apps/web/src/components/primitives/Pill.vue
+++ apps/web/src/components/primitives/Pill.vue
@@ -1,18 +1,34 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+
+const { size = 'regular' } = defineProps<{
+  size?: 'small' | 'regular'
+}>()
+
+const styles: HTMLAttributes['class'] = {
+  ['small']: size === 'small'
+}
+</script>
+
 <template>
-  <span class="pill">
+  <span class="pill" :class="styles">
     <slot></slot>
   </span>
 </template>
 
 <style scoped>
 .pill {
-  display: inline-block;
-  padding-inline: var(--spacing-sm);
-  padding-block: 2px;
+  display: inline-flex;
+  padding-inline: 0.5em;
+  padding-block: 0.1em;
   border-radius: var(--border-xs);
   background-color: var(--dark-6);
   color: var(--color-text-on-dark);
   font-weight: var(--font-weight-medium);
   font-size: var(--text-small);
+}
+
+.pill.small {
+  font-size: 0.75em;
 }
 </style>
