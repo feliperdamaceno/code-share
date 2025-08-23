02eff7eeb32cb77cfbfc5755226801b081c6fc8a
feliperdamaceno <feliperdamaceno@sent.com>
2025-08-23 19:40:57 +0100

    feat: resolve vue router on site build

--- apps/server/internal/core/routes.ts
+++ apps/server/internal/core/routes.ts
@@ -1,6 +1,10 @@
 import type { RouterInstance } from '../types/core.types.js'
 
+import fs from 'fs'
+import path from 'path'
+
 import { serveStatic } from '@hono/node-server/serve-static'
+import { HTTPException } from 'hono/http-exception'
 import { logger } from 'hono/logger'
 
 import { CategoryHandler } from '../handlers/categories.handler.js'
@@ -29,4 +33,23 @@ export function loadRoutes(router: RouterInstance) {
   router.route(`${prefix}/categories`, new CategoryHandler().router)
   router.route(`${prefix}/coupons`, new CouponHandler().router)
   router.route(`${prefix}/products`, new ProductsHandler().router)
+
+  /* route everything else to vue router */
+  router.all('*', async (ctx) => {
+    if (!/^\/(api|static|favicon\.ico)/.test(ctx.req.url)) {
+      try {
+        const index = path.resolve('./static/index.html')
+        const file = await fs.promises.readFile(index, 'utf-8')
+
+        ctx.header('Cache-Control', 'public, max-age=3600') /* 1 hour */
+        ctx.header('Content-Type', 'text/html')
+        return ctx.body(file)
+      } catch (error) {
+        console.error(error)
+        throw new HTTPException(500, {
+          message: 'Internal Server Error'
+        })
+      }
+    }
+  })
 }
